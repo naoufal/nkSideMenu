fdc87d9ff31972547dd9323aa512426d632d885c
Naoufal Kadhom <naoufalkadhom@gmail.com>
2013-06-22 12:08:13 -0400

    set css dir to "css" and images dir to "img"

--- config.rb
+++ config.rb
@@ -2,9 +2,9 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
+css_dir = "css"
 sass_dir = "sass"
-images_dir = "images"
+images_dir = "img"
 javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
