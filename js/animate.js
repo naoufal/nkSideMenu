c5d59c20ffb2a064b71991a33ae1019772f70560
Naoufal Kadhom <naoufalkadhom@gmail.com>
2013-06-24 16:16:13 -0400

    add animate to menu

--- js/animate.js
+++ js/animate.js
@@ -1,10 +1,21 @@
-$('.window').click(function(){
-	$('.window').addClass('animate');
-	$('.window').css('transform', 'translate3d(300px, 0, 0) scale(0.5,0.5) ');
+var deviceWidth;
+
+$(document).ready(function(){
+	deviceWidth = $(window).width();
 });
 
-$('.menu').click(function(){
+$('.button').click(function(){
+	$('.window').addClass('animate');
+	$('.menu').addClass('animate');
+	$('.window').css('transform', 'translate3d(' + (deviceWidth / 2) + 'px, 0px, 0) scale(0.5,0.5) ');
+	$('.menu').css('transform', 'scale(1,1) ');
+
+});
+
+$('.menu li').click(function(){
 	$('.window').css('transform', 'translate3d(0px, 0, 0) scale(1,1) ');
+	$('.menu').css('transform', 'scale(0.8,0.8) ');	
+	$('.title').html($(this).attr('class'));
 });
 
 function animate(){
