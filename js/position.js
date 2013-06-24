ec1e069587cf2a7be2fb9359ff6e225c239d7b51
Naoufal Kadhom <naoufalkadhom@gmail.com>
2013-06-24 16:49:02 -0400

    add function to vertically align elements

---
+++ js/position.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	verticalAlign('.menu-list');
+});
+
+function verticalAlign(element){
+	deviceHeight = $(window).height();
+	elementHeight = $(''+ element +'').height();
+	marginTop = (deviceHeight - elementHeight) / 2;
+	$(''+ element +'').css('margin-top', marginTop);
+}
