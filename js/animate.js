554a170aa6c9effe5b3bd7d3f55552595f0eadd8
Naoufal Kadhom <naoufalkadhom@gmail.com>
2013-07-09 22:04:50 -0400

    add modernizr logic to animate.js

--- js/animate.js
+++ js/animate.js
@@ -2,22 +2,39 @@ var deviceWidth;
 
 $(document).ready(function(){
 	deviceWidth = $(window).width();
+
+	$(function() {
+	    FastClick.attach(document.body);
+	});	
 });
 
-$('.button').click(function(){
-	$('.window').addClass('animate');
-	$('.menu').addClass('animate');
-	$('.window').css('transform', 'translate3d(' + (deviceWidth / 2) + 'px, 0px, 0) scale(0.5,0.5) ');
-	$('.menu').css('transform', 'scale(1,1) ');
-
+$('.side-menu-button').click(function(){
+	showSideMenu();
 });
 
 $('.menu li').click(function(){
-	$('.window').css('transform', 'translate3d(0px, 0, 0) scale(1,1) ');
-	$('.menu').css('transform', 'scale(0.8,0.8) ');	
-	$('.title').html($(this).attr('class'));
+	hideSideMenu();
 });
 
-function animate(){
+function showSideMenu(){
+	if(Modernizr.csstransitions){
+		$('.window').addClass('animate');
+		$('.menu').addClass('animate');
+		$('.window').css('transform', 'translate3d(' + (deviceWidth / 2) + 'px, 0px, 0) scale(0.5,0.5) ');
+		$('.menu').css('transform', 'scale(1,1) ');
+		$('.menu').css('opacity', '1');
+	} else {
+		alert('css transitions not supported');
+	}
+}
 
+function hideSideMenu(){
+	if(Modernizr.csstransitions){
+		$('.window').css('transform', 'translate3d(0px, 0, 0) scale(1,1) ');
+		$('.menu').css('transform', 'scale(0.6,0.6) ');	
+		$('.menu').css('opacity', '0');	
+		$('.title').html($(this).attr('class'));
+	} else {
+		alert('css transitions not supported');
+	}	
 }
